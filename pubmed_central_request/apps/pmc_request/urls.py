727d46e691be0219477e9f3a190164ce51b63755
Ross Blair <ross.pnd@gmail.com>
2015-08-31 09:38:49 -0700

    added some bootstrap styling to base template and request list table. removed asset directory from gitignore

--- pubmed_central_request/apps/pmc_request/urls.py
+++ pubmed_central_request/apps/pmc_request/urls.py
@@ -5,6 +5,6 @@ from pmc_request.views import PMCRequestForm, RequestDetail, RequestList
 urlpatterns = patterns('',
     url(r'^request/(?P<pk>\d+)$', RequestDetail.as_view(),
         name='request_detail'),
-    url(r'^request/all/$', RequestList.as_view()),
+    url(r'^request/all/$', RequestList.as_view(), name='request_list'),
     url(r'^$', PMCRequestForm.as_view(), name='request_create'),
 )
